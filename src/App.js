797e73ce60930f7965191771ff6db87c15115f57
ksrinreddy <ksrinreddy@gmail.com>
2023-05-18 18:26:45 +0530

    practice useState basics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React from "react";
+// ********* useState basics **********
+
+// import ErrorExample from "./tutorial/1-useState/1-error-example";
+// import UseStateBasics from "./tutorial/1-useState/2-useState-basics";
+// import UseStateArray from "./tutorial/1-useState/3-useState-array";
+// import UseStateObject from "./tutorial/1-useState/4-useState-object";
+import UseStateCounter from "./tutorial/1-useState/5-useState-counter";
+
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="container">
+      {/* ********** useState basics ********* */}
+      {/* <ErrorExample /> */}
+      {/* <UseStateBasics /> */}
+      {/* <UseStateArray /> */}
+      {/* <UseStateObject /> */}
+      <UseStateCounter />
+    </div>
+  );
 }
 
 export default App;
